c10100f30d42918799f46ada50e637f4872672ab
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-09-04 17:56:32 +0700

    add login ui

---
+++ resources/views/layout/source.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>paud</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/landing/img/favicon.png" rel="icon">
+    <link href="assets/landing/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('assets/landing/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/landing/vendor/aos/aos.css" rel="stylesheet')}}">
+    <link href="{{asset('assets/landing/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/landing/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/landing/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/landing/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/landing/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('assets/landing/css/style.css')}}" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Mentor - v4.8.1
+  * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    @yield('content')
+
+    <script src="assets/landing/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/landing/vendor/aos/aos.js"></script>
+    <script src="assets/landing/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/landing/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/landing/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/landing/js/main.js"></script>
+
+</body>
+
+</html>
