b7c0ae271b7931b329b4272b4f19c221cd98637e
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-09-04 19:57:26 +0700

    created migration

--- database/migrations/2022_09_04_122718_admin.php
+++ database/migrations/2022_09_04_122718_admin.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('username')->unique();
+            $table->string('password');
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('admin');
     }
 };
--- database/migrations/2022_09_04_122956_guru.php
+++ database/migrations/2022_09_04_122956_guru.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('guru', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nip')->unique();
+            $table->string('nama_guru');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('telepon');
+            $table->text('alamat');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('guru');
     }
 };
