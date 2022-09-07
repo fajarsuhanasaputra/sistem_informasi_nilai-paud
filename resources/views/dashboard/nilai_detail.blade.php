ccbe722f2d5a550bf7bcfab9b2c107c26c65f5a9
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-09-08 00:59:44 +0700

    fix iu in nilai detail

--- resources/views/dashboard/nilai_detail.blade.php
+++ resources/views/dashboard/nilai_detail.blade.php
@@ -8,74 +8,9 @@
         <span class="mask  bg-gradient-primary  opacity-6"></span>
     </div>
     <div class="card card-body mx-3 mx-md-4 mt-n6">
-        <div class="row gx-4 mb-2">
-            <div class="col-auto">
-                <div class="avatar avatar-xl position-relative">
-                    <img src="../assets/img/bruce-mars.jpg" alt="profile_image"
-                        class="w-100 border-radius-lg shadow-sm">
-                </div>
-            </div>
-            <div class="col-auto my-auto">
-                <div class="h-100">
-                    <h5 class="mb-1">
-                        Richard Davis
-                    </h5>
-                    <p class="mb-0 font-weight-normal text-sm">
-                        CEO / Co-Founder
-                    </p>
-                </div>
-            </div>
-        </div>
         <div class="row">
             <div class="row">
-                <div class="col-12 col-xl-3">
-                    <div class="card card-plain h-100">
-                        <div class="card-header pb-0 p-3">
-                            <div class="row">
-                                <div class="col-md-8 d-flex align-items-center">
-                                    <h6 class="mb-0">Biodata</h6>
-                                </div>
-                                <div class="col-md-4 text-end">
-                                    <a href="javascript:;">
-                                        <i class="fas fa-user-edit text-secondary text-sm" data-bs-toggle="tooltip"
-                                            data-bs-placement="top" title="Edit Profile"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body p-3">
-                            <p class="text-sm">
-                                Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally
-                                difficult paths, choose the one more painful in the short term (pain avoidance is
-                                creating an illusion of equality).
-                            </p>
-                            <hr class="horizontal gray-light my-4">
-                            <ul class="list-group">
-                                <li class="list-group-item border-0 ps-0 pt-0 text-sm"><strong class="text-dark">Full
-                                        Name:</strong> &nbsp; Alec M. Thompson</li>
-                                <li class="list-group-item border-0 ps-0 text-sm"><strong
-                                        class="text-dark">Mobile:</strong> &nbsp; (44) 123 1234 123</li>
-                                <li class="list-group-item border-0 ps-0 text-sm"><strong
-                                        class="text-dark">Email:</strong> &nbsp; alecthompson@mail.com</li>
-                                <li class="list-group-item border-0 ps-0 text-sm"><strong
-                                        class="text-dark">Location:</strong> &nbsp; USA</li>
-                                <li class="list-group-item border-0 ps-0 pb-0">
-                                    <strong class="text-dark text-sm">Social:</strong> &nbsp;
-                                    <a class="btn btn-facebook btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                                        <i class="fab fa-facebook fa-lg"></i>
-                                    </a>
-                                    <a class="btn btn-twitter btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                                        <i class="fab fa-twitter fa-lg"></i>
-                                    </a>
-                                    <a class="btn btn-instagram btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                                        <i class="fab fa-instagram fa-lg"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-xl-9">
+                <div class="col-12">
                     <div class="card card-plain h-100">
                         <div class="card-header pb-0 p-3">
                             <div class="row">
@@ -128,15 +63,6 @@
                                 </table>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-12 mt-4">
-                    <div class="mb-5 ps-3">
-                        <h6 class="mb-1">Daftar Nilai</h6>
-                        <p class="text-sm">Architects design houses</p>
-                    </div>
-                    <div class="row">
-
                     </div>
                 </div>
             </div>
