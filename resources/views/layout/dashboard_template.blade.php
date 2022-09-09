9beb33e73642a2e683bed7c3cfcc855d4c00225f
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-09-09 10:55:48 +0700

    change logo

--- resources/views/layout/dashboard_template.blade.php
+++ resources/views/layout/dashboard_template.blade.php
@@ -6,10 +6,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
-            target="_blank">
-            <img src="{{ asset('templates/dashboard/assets/img/logo-ct.png') }}" class="navbar-brand-img h-100"
-                alt="main_logo" />
+        <a class="navbar-brand m-0" href="/">
+            <img src="{{ asset('assets/paud/logo.png') }}" class="navbar-brand-img h-100" alt="main_logo" />
             <span class="ms-1 font-weight-bold text-white">PAUD Teratai</span>
         </a>
     </div>
--- resources/views/layout/source_dashboard.blade.php
+++ resources/views/layout/source_dashboard.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('templates/dashboard/assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('templates/dashboard/assets/img/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/paud/logo.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/paud/logo.png') }}">
     <title>
-        Material Dashboard 2 by Creative Tim
+        PAUD Teratai
     </title>
     <link rel="stylesheet" type="text/css"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
